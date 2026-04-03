6a23290828ef1710b3e2e8f1dca63106ee5af357
Erisa Sollova <erisasollova@gmail.com>
2026-04-03 19:26:38 +0200

    dimension + fact tables + procedura + exec

---
+++ sql/dwh.sql
@@ -0,0 +1,39 @@
+USE BigDataProject;
+GO
+
+CREATE TABLE DimCountry (
+    CountryKey INT IDENTITY(1,1) PRIMARY KEY,
+    Country_Code NVARCHAR(50) UNIQUE,
+    Country_Name NVARCHAR(255)
+);
+
+CREATE TABLE DimIndicator (
+    IndicatorKey INT IDENTITY(1,1) PRIMARY KEY,
+    Indicator_Code NVARCHAR(100) UNIQUE,
+    Indicator_Name NVARCHAR(255)
+);
+
+CREATE TABLE DimTime (
+    TimeKey INT PRIMARY KEY,
+    [Year] SMALLINT UNIQUE
+);
+
+CREATE TABLE FactEconomicData (
+    FactID INT IDENTITY(1,1) PRIMARY KEY,
+    CountryKey INT,
+    IndicatorKey INT,
+    TimeKey INT,
+    Value FLOAT,
+    FOREIGN KEY (CountryKey) REFERENCES DimCountry(CountryKey),
+    FOREIGN KEY (IndicatorKey) REFERENCES DimIndicator(IndicatorKey),
+    FOREIGN KEY (TimeKey) REFERENCES DimTime(TimeKey)
+);
+
+CREATE TABLE FactTrendAnalysis (
+    FactID INT IDENTITY(1,1) PRIMARY KEY,
+    CountryKey INT,
+    TimeKey INT,
+    AvgValue FLOAT,
+    FOREIGN KEY (CountryKey) REFERENCES DimCountry(CountryKey),
+    FOREIGN KEY (TimeKey) REFERENCES DimTime(TimeKey)
+);
---
+++ sql/etl.sql
@@ -0,0 +1,71 @@
+USE BigDataProject;
+GO
+
+CREATE OR ALTER PROCEDURE sp_LoadDWH
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO DimCountry (Country_Code, Country_Name)
+    SELECT DISTINCT Country_Code, Country_Name
+    FROM Staging_WorldBank s
+    WHERE NOT EXISTS (
+        SELECT 1 FROM DimCountry d
+        WHERE d.Country_Code = s.Country_Code
+    );
+
+    INSERT INTO DimIndicator (Indicator_Code, Indicator_Name)
+    SELECT DISTINCT Indicator_Code, Indicator_Name
+    FROM Staging_WorldBank s
+    WHERE NOT EXISTS (
+        SELECT 1 FROM DimIndicator d
+        WHERE d.Indicator_Code = s.Indicator_Code
+    );
+
+    INSERT INTO DimTime (TimeKey, [Year])
+    SELECT DISTINCT [Year], [Year]
+    FROM Staging_WorldBank s
+    WHERE NOT EXISTS (
+        SELECT 1 FROM DimTime d
+        WHERE d.[Year] = s.[Year]
+    );
+
+    INSERT INTO FactEconomicData (CountryKey, IndicatorKey, TimeKey, Value)
+    SELECT
+        dc.CountryKey,
+        di.IndicatorKey,
+        dt.TimeKey,
+        s.Value
+    FROM Staging_WorldBank s
+    JOIN DimCountry dc ON s.Country_Code = dc.Country_Code
+    JOIN DimIndicator di ON s.Indicator_Code = di.Indicator_Code
+    JOIN DimTime dt ON s.[Year] = dt.[Year]
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM FactEconomicData f
+        WHERE f.CountryKey = dc.CountryKey
+          AND f.IndicatorKey = di.IndicatorKey
+          AND f.TimeKey = dt.TimeKey
+    );
+
+    INSERT INTO FactTrendAnalysis (CountryKey, TimeKey, AvgValue)
+    SELECT
+        dc.CountryKey,
+        dt.TimeKey,
+        AVG(s.Value)
+    FROM Staging_WorldBank s
+    JOIN DimCountry dc ON s.Country_Code = dc.Country_Code
+    JOIN DimTime dt ON s.[Year] = dt.[Year]
+    WHERE s.Value IS NOT NULL
+    GROUP BY dc.CountryKey, dt.TimeKey
+    HAVING NOT EXISTS (
+        SELECT 1
+        FROM FactTrendAnalysis f
+        WHERE f.CountryKey = dc.CountryKey
+          AND f.TimeKey = dt.TimeKey
+    );
+END;
+GO
+
+
+EXEC sp_LoadDWH;
